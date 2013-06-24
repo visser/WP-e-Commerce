b25a1cfbcb575fa930c0c4fc1d2f4f4504edeb07
Pippin Williamson <pippin@pippinsplugins.com>
2013-06-24 13:20:46 -0500

    Combine meta data into single key. #406

--- wpsc-includes/class-rest-api.php
+++ wpsc-includes/class-rest-api.php
@@ -722,7 +722,8 @@ class WPSC_REST_API {
 			foreach ( $product_query as $p_object ) {
 
 				$custom = get_post_custom( $p_object->ID );
-
+				$product_metadata = ! empty( $custom['_wpsc_product_metadata'] ) ? maybe_unserialize( $custom['_wpsc_product_metadata'][0] ) : false;
+				//echo '<pre>'; print_r( $product_metadata ); echo '</pre>'; exit;
 				$products['products'][$i]['info']['id']                           = $p_object->ID;
 				$products['products'][$i]['info']['slug']                         = $p_object->post_name;
 				$products['products'][$i]['info']['title']                        = $p_object->post_title;
@@ -737,13 +738,7 @@ class WPSC_REST_API {
 				$products['products'][$i]['info']['categories']                   = wp_get_product_categories( $p_object->ID );
 				$products['products'][$i]['info']['stock']                        = $p_object->_wpsc_stock;
 				$products['products'][$i]['info']['sku']                          = $p_object->_wpsc_sku;
-				$products['products'][$i]['info']['taxable_amount']               = 0;
-				$products['products'][$i]['info']['external_link']                = array(
-					'url'    => ! empty( $custom['external_link']        ) ? maybe_unserialize( $custom['external_link']        ) : '',
-					'text'   => ! empty( $custom['external_link_text']   ) ? maybe_unserialize( $custom['external_link_text']   ) : '',
-					'target' => ! empty( $custom['external_link_target'] ) ? maybe_unserialize( $custom['external_link_target'] ) : ''
-				);
-
+				$products['products'][$i]['info']['metadata']                     = $product_metadata;
 				$products['products'][$i]['info']['featured_image']               = wp_get_attachment_url( get_post_thumbnail_id( $p_object->ID ) );
 				/*
 				 * TODO
